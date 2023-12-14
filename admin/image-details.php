6b866bc4fe4caec820c62a86b465771d9d7c46b7
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2023-12-14 19:24:15 +0530

    Create image-details.php

---
+++ admin/image-details.php
@@ -0,0 +1,8 @@
+<?php
+// Admin Image Details
+
+// Callback function for displaying image details and handling interactions in the admin dashboard
+function image_details_callback() {
+    // Your image details and interactions logic here
+}
+?>
