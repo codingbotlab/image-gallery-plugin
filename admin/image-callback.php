44b4c78ae3f71ff12cf28334f9b556836c350710
CodingBot <133736959+codingbotlab@users.noreply.github.com>
2023-12-14 19:21:00 +0530

    Create image-callback.php

---
+++ admin/image-callback.php
@@ -0,0 +1,8 @@
+<?php
+// Admin Image Callback
+
+// Callback function for image-related functionalities in the admin dashboard
+function image_callback() {
+    // Your image callback logic here
+}
+?>
